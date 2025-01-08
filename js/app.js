3d4ef51c040910b3b9a0c288928c6992ff9befc1
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-08 08:22:35 -0300

    recuperando quantidades de cada tipo de ingresso

--- js/app.js
+++ js/app.js
@@ -5,5 +5,11 @@ function comprar(){
     let ingresso = document.getElementById('tipo-ingresso').value;
     let quantidade = document.getElementById('qtd').value;
 
+    let pista = document.getElementById('qtd-pista').textContent;
+    let superior = document.getElementById('qtd-superior').textContent;
+    let inferior = document.getElementById('qtd-inferior').textContent;
+
     console.log(`Ingresso: ${ingresso} - Quantidade: ${quantidade}`);
+
+    console.log(`${pista}, ${superior}, ${inferior}`);
 }
