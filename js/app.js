c60b87c777ee164d6a5d58d39653d159c4b9e18b
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-08 08:14:25 -0300

    Recuperando valores do tipo de ingresso e da quantida informada

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+
+function comprar(){
+
+    let ingresso = document.getElementById('tipo-ingresso').value;
+    let quantidade = document.getElementById('qtd').value;
+
+    console.log(`Ingresso: ${ingresso} - Quantidade: ${quantidade}`);
+}
