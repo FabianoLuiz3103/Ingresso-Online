332b087ad8fc4514b9921e270f39016eb34835ab
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-08 09:01:04 -0300

    criando funcionalidade de alterar quantidade de ingresso na lista e validações

--- js/app.js
+++ js/app.js
@@ -1,15 +1,58 @@
 
 
-function comprar(){
+function comprar() {
 
     let ingresso = document.getElementById('tipo-ingresso').value;
     let quantidade = document.getElementById('qtd').value;
 
-    let pista = document.getElementById('qtd-pista').textContent;
-    let superior = document.getElementById('qtd-superior').textContent;
-    let inferior = document.getElementById('qtd-inferior').textContent;
+    let tagPista = document.getElementById('qtd-pista');
+    let tagSuperior = document.getElementById('qtd-superior');
+    let tagInferior = document.getElementById('qtd-inferior');
 
-    console.log(`Ingresso: ${ingresso} - Quantidade: ${quantidade}`);
+    let vlPista = parseInt(document.getElementById('qtd-pista').textContent);
+    let vlSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    let vlInferior = parseInt(document.getElementById('qtd-inferior').textContent);
 
-    console.log(`${pista}, ${superior}, ${inferior}`);
+    switch (ingresso) {
+        case 'pista':
+            if (!isNotDisponivel(vlPista, ingresso) && !isQuantidadeInvalid(vlPista, quantidade, ingresso)) {
+                vlPista = vlPista - quantidade;
+                tagPista.textContent = vlPista
+            }
+            ;
+            break;
+        case 'superior':
+            if (!isNotDisponivel(vlSuperior, ingresso) && !isQuantidadeInvalid(vlSuperior, quantidade, ingresso)) {
+            vlSuperior = vlSuperior - quantidade;
+            tagSuperior.textContent = vlSuperior;
+            }
+            break;
+        case 'inferior':
+            if (!isNotDisponivel(vlInferior, ingresso) && !isQuantidadeInvalid(vlInferior, quantidade, ingresso)) {
+            vlInferior = vlInferior - quantidade;
+            tagInferior.textContent = vlInferior;
+            }
+            break;
+    }
 }
+
+function isNotDisponivel(vlAtualLista, tipo) {
+
+    if (vlAtualLista <= 0) {
+
+        alert(`NÃO HÁ MAIS INGRESSOS DISPONÍVEIS PARA A MODALIDADE: ${tipo.toUpperCase()}`);
+        return true;
+    }
+}
+
+function isQuantidadeInvalid(vlAtualLista, valorInformado, tipo) {
+    if (valorInformado > vlAtualLista) {
+
+        alert(`A QUANTIDADE: ${valorInformado} É INVÁLIDA! PARA A MODALIDADE: ${tipo.toUpperCase()} HÁ ${vlAtualLista} INGRESSOS DISPONÍVEIS.`);
+        return true;
+    } else if (valorInformado <= 0){
+        alert(`A QUANTIDADE INFORMADA DEVE SER MAIOR QUE ZERO!! `);
+        return true;
+    }
+}
+
